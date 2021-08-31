b5fdda4fdb02773a283a87e98b3f17d9531fccdc
LiYu <2248868140@qq.com>
2021-08-31 16:55:27 +0800

    添加svg文件

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,24 @@ import {
     Route,
     Link,
     Redirect
-} from "react-router-dom";
+} from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
-const Wrapper=styled.div`
+
+
+const Wrapper = styled.div`
   border: 1px solid red;
   display: flex;
   flex-direction: column;
   height: 100vh;
-`
-const Content=styled.div`
- flex-grow: 1;
+`;
+const Content = styled.div`
+  flex-grow: 1;
   overflow: auto;
-`
-const Ul=styled.ul`
-  display: flex;
-   li{
-     width: 33.3%;
-     border: 1px solid blue ;
-     display: flex;
-     justify-content: center;
-     align-items: center;
-   }
-`
+`;
+
+
 function App() {
     return (
         <Router>
@@ -45,23 +40,11 @@ function App() {
                         </Route>
                         <Redirect exact from="/" to="/money"/> {/*默认路径*/}
                         <Route path="*">
-                            <NoMatch />
+                            <NoMatch/>
                         </Route>
                     </Switch>
                 </Content>
-                <nav>
-                    <Ul>
-                        <li>
-                            <Link to="/labels">标签</Link>
-                        </li>
-                        <li>
-                            <Link to="/money">记账</Link>
-                        </li>
-                        <li>
-                            <Link to="/statistics">统计</Link>
-                        </li>
-                    </Ul>
-                </nav>
+                <Nav />
             </Wrapper>
         </Router>
     );
@@ -78,8 +61,9 @@ function Labels() {
 function Statistics() {
     return <h2>统计</h2>;
 }
-function  NoMatch(){
-    return <div>页面不存在</div>
+
+function NoMatch() {
+    return <div>页面不存在</div>;
 }
 
-export default App
+export default App;
